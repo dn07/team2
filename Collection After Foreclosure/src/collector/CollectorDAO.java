d8899fc60e54c458a7f3bedf19faea59dae00070
dn07 <deepakmnahar@gmail.com>
2020-05-26 22:20:55 +0530

    Bugs Fixes for Collector Pages

--- Collection After Foreclosure/src/collector/CollectorDAO.java
+++ Collection After Foreclosure/src/collector/CollectorDAO.java
@@ -4,4 +4,6 @@ public interface CollectorDAO {
 	public int insertCollector(Collector c);
 
 	public Collector getCollector(String cid, String lid, double amount);
+
+	public Collector getCollector(String cid, String lid);
 }
--- Collection After Foreclosure/src/collector/CollectorDAOImpl.java
+++ Collection After Foreclosure/src/collector/CollectorDAOImpl.java
@@ -50,4 +50,10 @@ public class CollectorDAOImpl implements CollectorDAO {
 		}
 		return c;
 	}
+
+	@Override
+	public Collector getCollector(String cid, String lid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
