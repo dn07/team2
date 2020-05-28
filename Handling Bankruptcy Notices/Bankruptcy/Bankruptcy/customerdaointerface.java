d7f29e61a4eb9b08f087c5e4d14b873dbfd35aa1
adi408116 <44616910+adi408116@users.noreply.github.com>
2020-05-28 23:17:43 +0530

    Delete customerdaointerface.java

--- Handling Bankruptcy Notices/Bankruptcy/Bankruptcy/customerdaointerface.java
+++
@@ -1,7 +0,0 @@
-package Bankruptcy;
-
-public interface customerdao {
-public int insertcustomer(customer c);	
-public customer getcustomer(String FirstName,String LastName,String LoanId, String BankruptcyId);
-
-}
